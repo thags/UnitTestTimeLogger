f5a313e9a347c2e65813c0e75bd8ea2bd5536936
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2022-01-19 21:49:42 -0600

    Add the start of unit testing

---
+++ LoggerTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LoggerTests
+{
+    [TestClass]
+    public class DateTests
+    {
+        [TestMethod]
+        public void SubtractFromTodayTests()
+        {
+        }
+    }
+}
